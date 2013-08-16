45d65a7b18b928ab59552a1d014a2cdb506f820f
Yui <t-kasuv@microsoft.com>
2013-08-16 16:49:29 -0700

    Update commit for postCompilationProcessor

--- src/compiler/postcompilation/postcompilationProcessor.ts
+++ src/compiler/postcompilation/postcompilationProcessor.ts
@@ -11,12 +11,15 @@ module TypeScript {
         }
     }
 
-    /*
-    */
+    // This class is for performing various kinds of post-compilation analysis.
+    // An object of this class is created per document. Each object contains its own
+    // array of sub-processor which specified by users through CompilationSettings.
     export class PostCompilationProcessor {
         private _processors: SubProcessor[] = null;
         private _diagnostics: Diagnostic[] = null;
 
+        // Constructor takes what Document to perform post-compilation process
+        // and CompilationSetting which specifies what type of sub-process to be done.
         constructor(public document: Document, settings: CompilationSettings) {
             this._processors = [];
             this._diagnostics = [];
